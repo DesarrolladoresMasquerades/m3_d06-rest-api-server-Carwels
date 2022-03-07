17ff062edec9e8ae57ae5d87aa667b7b5aca173c
Carwi Barrios <carwelsbarrios@gmail.com>
2022-03-07 13:46:11 +0100

    class

---
+++ models/Project.model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const projectSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, maxLength: 500 },
+  // owner: { type: Schema.Types.ObjectId, ref: "User" },
+  task: [{ type: Schema.Types.ObjectId, ref: "Task", default: [] }],
+});
+
+module.exports = model("Project", projectSchema);
---
+++ routes/project.routes.js
@@ -0,0 +1,44 @@
+const router = require("express").Router();
+const mongoose = require("mongoose");
+
+const Project = require("../models/Project.model");
+
+router.get("/:projectId", (req, res) => {
+  const { projectId } = req.params;
+
+  Project.findById(projectId)
+    .populate("tasks")
+    .then((project) => res.json(project))
+    .catch((error) => res.json(error));
+});
+
+router.put("/:projectId", (req, res) => {
+  const { projectId } = req.params;
+
+  Project.findByIdAndUpdate(projectId, req.body, { new: true })
+    .then((updatedProject) => res.json(updatedProject))
+    .catch((error) => res.json(error));
+});
+
+router.delete("/:projectId", (req, res) => {
+  const { projectId } = req.params;
+
+  Project.findByIdAndRemove(projectId)
+    .then((deletedProject) => res.json(deletedProject))
+    .catch((error) => res.json(error));
+});
+
+router.get("/", (req, res) => {
+  Project.find()
+    .populate("tasks")
+    .then((projects) => res.json(projects))
+    .catch((error) => res.json(error));
+});
+
+router.post("/", (req, res) => {
+  Project.create(req.body)
+    .then((newProject) => res.json(newProject))
+    .catch((error) => res.json(error));
+});
+
+module.exports = router;
